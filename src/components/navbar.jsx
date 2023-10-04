a659cc5d6d79712e2ab105fc21ca7f087096141c
Zakaria Farah <zakaria.farah99@gmail.com>
2023-10-03 20:12:28 -0400

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
+
+
+
+export const Navbar = () => {
+    return 
+    <div className = "navbar">
+        <div className='links'>
+
+        </div>
+    </div>
+}
